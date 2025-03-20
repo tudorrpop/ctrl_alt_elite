5683cc5c73de6ab4fc8fe09b528b11d051678e95
Adrian Pop <101561449+adrianpop3@users.noreply.github.com>
2025-03-20 22:37:10 +0200

    Backend setup added.

---
+++ SmartParkSwarm-Back/src/test/java/smartParkSwarm/backend/SmartParkSwarm_Back/SmartParkSwarmBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package smartParkSwarm.backend.SmartParkSwarm_Back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartParkSwarmBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
