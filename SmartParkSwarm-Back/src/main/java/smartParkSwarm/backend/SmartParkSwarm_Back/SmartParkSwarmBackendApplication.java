43bccabaebbeb1c1809827fb0a8682367582f098
Adrian Pop <adrian.pop2@student.upt.ro>
2025-03-21 22:10:28 +0200

    Added the desired structure.

---
+++ SmartParkSwarm-Back/src/main/java/smartParkSwarm/backend/SmartParkSwarm_Back/SmartParkSwarmBackendApplication.java
@@ -0,0 +1,13 @@
+package smartParkSwarm.backend.SmartParkSwarm_Back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartParkSwarmBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartParkSwarmBackendApplication.class, args);
+	}
+
+}
