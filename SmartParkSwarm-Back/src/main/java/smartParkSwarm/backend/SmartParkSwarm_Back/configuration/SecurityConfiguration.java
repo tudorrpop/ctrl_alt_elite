eb810f7bb7c8aba44dda2793586d2574316267d7
MergeaSergiu <mergeasergiu@gmail.com>
2025-06-01 17:07:28 +0300

    rollback SecurityConfiguration

--- SmartParkSwarm-Back/src/main/java/smartParkSwarm/backend/SmartParkSwarm_Back/configuration/SecurityConfiguration.java
+++ SmartParkSwarm-Back/src/main/java/smartParkSwarm/backend/SmartParkSwarm_Back/configuration/SecurityConfiguration.java
@@ -31,7 +31,7 @@ public class SecurityConfiguration {
         return http
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/register", "/login", "/sse", "/stores/*", "customer/*", "admin/*", "worker/**").permitAll()
+                        .requestMatchers("/register", "/login", "/sse", "worker/**", "/stores/**", "customer/**", "admin/**").permitAll()
                         .anyRequest().authenticated()
                 )
                 .exceptionHandling(exception -> exception
